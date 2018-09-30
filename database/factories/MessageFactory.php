d2b4aab18f3c079f9f4a21ecd897ca8c05aec763
Jhonny Córdova <jcordova@borealis.cl>
2018-09-30 10:11:11 -0300

    Commit inicial

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Message::class, function (Faker $faker) {
+    return [
+		'content' => $faker->realText(random_int(20, 160)),
+		'image' => $faker->imageUrl(600, 338),
+		// 'created_at' => $faker->dateTime,
+		// 'updated_at' => $faker->dateTime,
+	];
+});
---
+++ resources/js/components/Responses.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="row">
+		<a href="#" v-on:click="load">Ver Respuestas</a>
+		<div class="card mt-2" v-for="response in responses">
+			<div class="card-block">
+				{{ response.message }}
+			</div>
+			<div class="card-footer text-muted">
+				{{ response.created_at }}
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		props: ['message'],
+		data(){
+			return {
+				responses: []
+			}
+		},
+
+		methods:{
+			load(){
+				axios.get('/api/messages/'+ this.message +'/responses')
+					.then(res => {
+						this.responses = res.data;
+					});
+			}
+		}
+	}
+</script>
